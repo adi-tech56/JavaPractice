3499b3408bb50fdc1177c7f7b2eb447e962914d6
Adishree <adishree@argusoft.com>
2025-08-04 10:38:37 +0530

    JDBC

--- src/company/Main.java
+++ src/company/Main.java
@@ -2,15 +2,26 @@ package company;
 
 
 import java.sql.Connection;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         NewSqlite db = new NewSqlite();
-       Connection conn= db.connect_to_db("Jdbc","postgres","argusadmin");
+        Scanner s = new Scanner(System.in);
+        System.out.println("enter the username");
+        String name = s.nextLine();
+        System.out.println("enter the password");
+        String pass = s.nextLine();
+        Connection conn= db.connect_to_db("Jdbc",name,pass);
+        System.out.println("Enter the option  1 for create,2 for update,3 for read or 4 for delete");
+
 //        db.createTable(conn,"employee_table");
 //        db.insert_row(conn,"employee_table","Adi","Cuttack");
 //        db.insert_row(conn,"employee_table","Adishree","Cuttack");
-        db.read_data(conn,"employee_table");
+//        db.read_data(conn,"employee_table");
+//        db.update_name(conn,"employee_table","Adi","Shree");
+//        db.read_data(conn,"employee_table");
+        db.serch_by_name(conn,"employee_table","Shree");
     }
 
 }
--- src/company/NewSqlite.java
+++ src/company/NewSqlite.java
@@ -68,4 +68,34 @@ statement.executeUpdate(query);
             System.out.println(e);
         };
     }
+    public void update_name(Connection conn,String table_name,String old_name,String new_name){
+        Statement statement;
+        try{
+            String query = String.format("UPDATE %s set name ='%s' where emp_id = 1",table_name,new_name,old_name);
+            statement = conn.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Data updated");
+
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+    public void serch_by_name(Connection conn,String table_name,String name){
+        Statement statement;
+        ResultSet rs= null;
+        try{
+            String query = String.format("SELECT * from %s where name ='%s'",table_name,name);
+            statement = conn.createStatement();
+             rs = statement.executeQuery(query);
+            while(rs.next()){
+                System.out.print(rs.getString("emp_id"));
+                System.out.print("   ");
+                System.out.print(rs.getString("name"));
+                System.out.print("   ");
+                System.out.println(rs.getString("address"));
+            }
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
 }
